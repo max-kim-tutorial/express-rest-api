36bc27c57f94cc508dc021dd01d493fe6bafd941
Max Kim <hwaseen@gmail.com>
2020-06-08 10:32:39 +0900

    first api server

---
+++ server2.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+const users = [
+  { id: 1, name: "alice", age: 24 },
+  { id: 2, name: "max", age: 21 },
+  { id: 3, name: "sandy", age: 29 },
+];
+app.use(morgan("dev"));
+
+app.get("/users", function (req, res) {
+  res.json(users);
+});
+
+app.listen(3000, function () {
+  console.log("server running");
+});
