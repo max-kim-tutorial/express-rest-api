96299f83432762dc850c5ebe692682948a716f5d
Max Kim <hwaseen@gmail.com>
2020-07-01 17:34:56 +0900

    Add API for updating user info with test case

--- server2.js
+++ server2.js
@@ -47,6 +47,24 @@ app.get("/users/:id", function (req, res) {
   res.json(user);
 });
 
+// UPDATE
+app.put("/users/:id", (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  if (Number.isNaN(id)) return res.status(400).end();
+
+  const name = req.body.name;
+  if (!name) return res.status(400).end();
+
+  const isConflict = users.filter((user) => user.name === name).length;
+  if (isConflict) return res.status(409).end();
+
+  const user = users.filter((user) => user.id === id)[0];
+  if (!user) return res.status(404).end();
+
+  user.name = name;
+  res.json(user);
+});
+
 // DELETE
 app.delete("/users/:id", (req, res) => {
   const id = parseInt(req.params.id, 10);
--- test/server2.spec.js
+++ test/server2.spec.js
@@ -96,6 +96,44 @@ describe("POST /users", () => {
     });
   });
 
+  // Update
+  describe("PUT /uesers/:id", () => {
+    describe("성공시", () => {
+      it("변경된 name을 응답한다", (done) => {
+        const name = "charlie";
+        request(app)
+          .put("/users/3")
+          .send({ name })
+          .end((err, res) => {
+            res.body.should.have.property("name", name);
+            done();
+          });
+      });
+    });
+    describe("실패시", () => {
+      it("정수가 아닌 id일 경우 400을 응답", (done) => {
+        request(app).put("/users/one").expect(400).end(done);
+      });
+      it("name이 없는 경우 400을 응답", (done) => {
+        request(app).put("/users/1").send({}).expect(400).end(done);
+      });
+      it("없는 유저일 경우 404를 응답", (done) => {
+        request(app)
+          .put("/users/999")
+          .send({ name: "foo" })
+          .expect(404)
+          .end(done);
+      });
+      it("이름이 중복일 경우 409를 응답", (done) => {
+        request(app)
+          .put("/users/3")
+          .send({ name: "max" })
+          .expect(409)
+          .end(done);
+      });
+    });
+  });
+
   // Delete
   describe("DELETE /users/1는", () => {
     describe("성공시", () => {
