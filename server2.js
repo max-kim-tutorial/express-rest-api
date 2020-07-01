cbba736afdb05753898a34cd0ae9558073efc81b
Max Kim <hwaseen@gmail.com>
2020-07-01 15:52:38 +0900

    Add API reading user info also test case

--- server2.js
+++ server2.js
@@ -10,7 +10,24 @@ const users = [
 app.use(morgan("dev"));
 
 app.get("/users", function (req, res) {
-  res.json(users);
+  req.query.limit = req.query.limit || 10;
+  const limit = parseInt(req.query.limit, 10);
+  if (Number.isNaN(limit)) {
+    return res.status(400).end();
+  }
+  res.json(users.slice(0, limit));
+});
+
+app.get("/users/:id", function (req, res) {
+  const id = parseInt(req.params.id, 10);
+  if (Number.isNaN(id)) {
+    return res.status(400).end();
+  }
+  const user = users.filter((user) => user.id === id)[0];
+  if (!user) {
+    return res.status(404).end();
+  }
+  res.json(user);
 });
 
 app.listen(3000, function () {
