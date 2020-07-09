6144f61b5c4f35c44f11a9ada169dfa68686d832
Max Kim <hwaseen@gmail.com>
2020-07-09 17:31:00 +0900

    Deleted bin folder

--- src/bin/sync-db.js
+++
@@ -1,8 +0,0 @@
-const models = require("../models");
-
-module.exports = () => {
-  const options = {
-    force: process.env.NODE_ENV === "test" ? true : false,
-  };
-  return models.sequelize.sync(options);
-};
--- src/bin/www.js
+++
@@ -1,9 +0,0 @@
-const app = require("../app");
-const syncDb = require("./sync-db");
-
-syncDb().then(() => {
-  console.log("디비싱크");
-  app.listen(3000, () => {
-    console.log("server start");
-  });
-});
